da142467dd13405f5c1da31364e81b402025eaf7
nikola <savicn209@gmail.com>
2023-08-30 23:41:03 +0200

    upd on blog 2

---
+++ custom/plugins/CustomBlogPlugin/src/Resources/app/administration/src/module/custom-blog-plugin/component/CustomBlogList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h1>{{ $t('custom-blog-plugin.general.title') }}</h1>
+      <table class="sw-data-grid">
+        <thead>
+          <tr>
+            <th>{{ $t('custom-blog-plugin.list.title') }}</th>
+            <th>{{ $t('custom-blog-plugin.list.publishDate') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="post in posts" :key="post.id">
+            <td>{{ post.title }}</td>
+            <td>{{ formatDate(post.publishDate) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CustomBlogList',
+    props: {
+      posts: {
+        type: Array,
+        required: true,
+      },
+    },
+    methods: {
+      formatDate(date) {
+        return new Date(date).toLocaleDateString();
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* Your CSS styles for the component */
+  </style>
+  
